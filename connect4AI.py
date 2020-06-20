953a707dc9fb3838a0007407a6335661ec380a37
Lunaticsatoshi <deysarkersudeep@gmail.com>
2020-06-20 16:56:01 +0530

    Implementing the base for AI

--- connect4AI.py
+++ connect4AI.py
@@ -15,6 +15,7 @@ AI = 1
 
 WINDOW_LENGTH = 4
 
+EMPTY = 0
 PLAYER_PIECE = 1
 AI_PIECE = 2
 
@@ -81,37 +82,79 @@ class Board:
                 if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
                     return True
 
-    def score_position(self, board, piece):
-        ## Horizontal Score
+    def evaluate_win(self, window, piece):
         score = 0
+        opp_piece = PLAYER_PIECE
+        if piece == PLAYER_PIECE:
+            opp_piece = AI_PIECE
+
+        if window.count(piece) == 4:
+            score += 100
+        elif window.count(piece) == 3 and window.count(EMPTY) == 1:
+            score += 10
+        elif window.count(piece) == 2 and window.count(EMPTY) == 2:
+            score += 5
+
+        if window.count(opp_piece) == 3 and window.count(EMPTY) == 1:
+            score -= 80
+
+        return score
+
+    def score_position(self, board, piece):
+        score = 0
+        # Center Score
+        center_array = [int(i) for i in list(board[:, COLUMN_COUNT//2])]
+        center_count = center_array.count(piece)
+        score += center_count * 6
+        
+        ## Horizontal Score
         for r in range(ROW_COUNT):
             row_array = [int(i) for i in list(board[r,:])]
             for c in range(COLUMN_COUNT-3):
                 window = row_array[c:c+WINDOW_LENGTH]
-                if window.count(piece) == 4:
-                    score += 100
+                score += self.evaluate_win(window, piece)
 
-                elif: window.count(piece) == 3 and window.count(EMPTY) == 1:
-                    score += 10
+        ## Vertical Score
+        for c in range(COLUMN_COUNT):
+            col_array = [int(i) for i in list(board[:,c])]
+            for r in range(ROW_COUNT-3):
+                window = col_array[r:r+WINDOW_LENGTH]
+                score += self.evaluate_win(window, piece)
+
+        ## Diagonal Score
+        for r in range(ROW_COUNT-3):
+            for c in range(COLUMN_COUNT-3):
+                window = [board[r+i][c+i] for i in range(WINDOW_LENGTH)]
+                score += self.evaluate_win(window, piece)
+
+        for r in range(ROW_COUNT-3):
+            for c in range(COLUMN_COUNT-3):
+                window = [board[r+3-i][c+i] for i in range(WINDOW_LENGTH)]
+                score += self.evaluate_win(window, piece)
 
         return score
 
     def get_valid_locations(self, board):
         valid_locations = []
         for col in range(COLUMN_COUNT):
-            if is_valid_location(board, col):
+            if self.is_valid_location(board, col):
                 valid_locations.append(col)
         return valid_locations
 
     def pick_best_move(self, board, piece):
-        valid_locations = get_valid_locations(board)
-        best_score = 0
+        valid_locations = self.get_valid_locations(board)
+        best_score = -10000
         best_col = random.choice(valid_locations)
         for col in valid_locations:
-            row = get_next_open_row(board, col)
+            row = self.get_next_open_row(board, col)
             temp_board = board.copy()
-            drop_piece(temp_board, row, col, piece)
-            score = score_position(temp_board, piece)
+            self.drop_piece(temp_board, row, col, piece)
+            score = self.score_position(temp_board, piece)
+            if score > best_score:
+                best_score = score
+                best_col = col
+
+        return best_col
                 
 
     def draw_board(self, board):
@@ -187,7 +230,7 @@ if __name__ == "__main__":
         #Ask for Player 2
         if turn == AI and not game_over:
             
-            col = random.randint(0, COLUMN_COUNT-1)
+            col = board.pick_best_move(game, AI_PIECE)
 
             if board.is_valid_location(game, col):
                 pygame.time.wait(500)
